790eddce61b31f4563ee15bcb9ed9829eec0844b
Willie Owens <willie.owens91@gmail.com>
2020-06-08 21:57:50 -0400

    Styling for User Course list

--- app/src/main/java/com/frolfr/ui/courses/CourseAdapter.kt
+++ app/src/main/java/com/frolfr/ui/courses/CourseAdapter.kt
@@ -2,12 +2,17 @@ package com.frolfr.ui.courses
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.frolfr.R
 import com.frolfr.api.model.Course
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 
 class CourseAdapter : RecyclerView.Adapter<CourseItemViewHolder>() {
+
+    var isoDateFormat: DateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    var df: DateFormat = SimpleDateFormat("MM/dd/yy")
 
     var courses = listOf<Course>()
         set(value) {
@@ -19,13 +24,16 @@ class CourseAdapter : RecyclerView.Adapter<CourseItemViewHolder>() {
 
     override fun onBindViewHolder(holder: CourseItemViewHolder, position: Int) {
         val course = courses[position]
-        holder.textView.text = course.name
+        holder.name.text = course.name
+        holder.location.text = course.location
+        holder.holeCount.text = course.holeCount.toString()
+        holder.lastPlayed.text = df.format(isoDateFormat.parse(course.lastPlayedAt))
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseItemViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater.inflate(R.layout.user_course_view, parent, false)
-        return CourseItemViewHolder(view as TextView)
+        return CourseItemViewHolder(view as ConstraintLayout)
     }
 
 }
--- app/src/main/java/com/frolfr/ui/courses/CourseItemViewHolder.kt
+++ app/src/main/java/com/frolfr/ui/courses/CourseItemViewHolder.kt
@@ -1,6 +1,13 @@
 package com.frolfr.ui.courses
 
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.frolfr.R
 
-class CourseItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+class CourseItemViewHolder(val layout: ConstraintLayout) : RecyclerView.ViewHolder(layout) {
+    val name = layout.findViewById<TextView>(R.id.user_course_name)
+    val location = layout.findViewById<TextView>(R.id.user_course_location)
+    val holeCount = layout.findViewById<TextView>(R.id.user_course_hole_count)
+    val lastPlayed = layout.findViewById<TextView>(R.id.user_course_last_played)
+}
